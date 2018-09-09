365f28534b9e2ed98035d4f1d4bdb290c848d01b
lijian <852688838@qq.com>
2018-09-09 15:28:28 +0800

    初始化

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel!hello summer
+                    Laravel!hello world
                 </div>
 
                 <div class="links">
