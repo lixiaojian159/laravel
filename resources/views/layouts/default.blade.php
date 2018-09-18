e859f97c1ce97a5069a84856991a6a569ede681e
lijian <852688838@qq.com>
2018-09-18 21:41:40 +0800

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" href="/css/app.css">
 	<title>@yield('title','Sample App')- Laravel 新手入门教程</title>
 </head>
 <body>
-	@yield('content')
+	@include('layouts._header')
+	<div class="container">
+        @yield('content')
+        @include('layouts._footer')
+    </div>
 </body>
 </html>
